de04577ff75bb2834f353bce255c91dd333d22b3
Calon Dosen Muda <aksabrata251@gmail.com>
2026-09-22 12:57:29 +0700

    Menambahkan Challenge 2 soal 1

--- Challenge2_b11.js
+++ Challenge2_b11.js
@@ -1,7 +1,34 @@
 const taskInput = document.getElementById("taskInput");
 const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
 
 addButton.addEventListener("click", function() {
     const taskText = taskInput.value;
-    console.log(taskText);
+
+    if (taskText === "") {
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    const taskSpan = document.createElement("span");
+    taskSpan.textContent = taskText;
+
+    taskSpan.addEventListener("click", function() {
+        taskSpan.style.textDecoration = "line-through";
+    });
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Hapus";
+
+    deleteButton.addEventListener("click", function() {
+        li.remove();
+    });
+
+    li.appendChild(taskSpan);
+    li.appendChild(deleteButton);
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
 });
